a50d961bc2c48f6daa714b9cfab9c8a481c5b456
Gideon Kalanzi <gidinsambu@gmail.com>
2026-07-18 10:46:40 +0300

    added names

--- guests.js
+++ guests.js
@@ -4,6 +4,13 @@
   can access the site files. See README.md for the privacy note.
 */
 const guests = {
-  "gideon-kalanzi": "Gideon Kalanzi",
-  "guest-of-honour": "Guest of Honour"
+  "mr-sali-charles": "Mr Sali Charles",
+  "mr-and-mrs-jjuuko": "Mr and Mrs Jjuuko",
+  "mr-and-mrs-mawejje": "Mr and Mrs Mawejje",
+  "mr-and-mrs-lutakome": "Mr and Mrs Lutakome",
+  "mr-and-mrs-pat-we": "Mr and Mrs Pat we",
+  "mr-and-mrs-kiggundu": "Mr and Mrs Kiggundu",
+  "mr-and-mrs-bwize": "Mr and Mrs Bwize",
+  "mr-and-mrs-mukalazi": "Mr and Mrs Mukalazi",
+  "mr-and-mrs-ssentongo": "Mr and Mrs Ssentongo"
 };
